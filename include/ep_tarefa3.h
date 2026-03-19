fb76029de6a1b0e02c2b728e11e883644c690741
HenriqueSantiagoUSP <henrique_santiago@usp.br>
2026-03-19 15:13:46 -0300

    ep_tarefa3 v2

--- include/ep_tarefa3.h
+++ include/ep_tarefa3.h
@@ -4,6 +4,18 @@ FUNÇÕES DA TAREFA EP_TAREFA3
 #ifndef EP_TAREFA3_H
 #define EP_TAREFA3_H
 
+//===========================================================================
+/* Resolve Sistema trinagular superior
+
+Parâmetros:
+- N: Dimensão da matriz R e tamanho dos vetores b e x
+- R: Matriz triangular superior
+- b: Vetor independente
+- x: Vetor das incógnitas
+
+*/
 void resolver_sistema_triangular_superior(int N, double R[N][N], double b[N], double x[N]);
+
+//===========================================================================
 
 #endif
