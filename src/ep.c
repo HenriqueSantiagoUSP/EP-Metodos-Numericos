1267a3c29542d0eaed6df832abf9df4ef893da3b
HenriqueSantiagoUSP <henrique_santiago@usp.br>
2026-03-19 17:50:24 -0300

    Include tarefa3 no ep.c

--- src/ep.c
+++ src/ep.c
@@ -1,13 +1,13 @@
 /*
 FILE DESTINADO A IMPLEMENTAÇÃO DO CÓDIGO FINAL DO EP
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "random.h"
 #include "ep_tarefa1.h"
 #include "ep_tarefa2.h"
+#include "ep_tarefa3.h"
 
 int main(){
 
